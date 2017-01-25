c6011e42f915a4a627c53293061338286646d6c6
Jason R. Coombs <jaraco@jaraco.com>
2017-01-25 09:51:10 -0500

    Fix test failures on Python 2.6

--- tests/test_properties.py
+++ tests/test_properties.py
@@ -5,9 +5,9 @@ def test_AutoBindingMethod():
 	class Example(object):
 		@properties.simplemethod
 		def method(self):
-			return 'called with {!r}'.format(self)
+			return 'called with {self!r}'.format(self=self)
 
 	ex = Example()
-	assert ex.method().startswith('called with {!r}'.format(ex))
+	assert ex.method().startswith('called with {ex!r}'.format(ex=ex))
 
 	assert Example.method('some value') == "called with 'some value'"
